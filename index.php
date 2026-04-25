9cbf6babf80d73a8838f92735e4e8980e727c86b
nadyagildas <nadyagildas0713@gmail.com>
2026-04-25 21:32:27 +0700

    ngisi index

--- index.php
+++ index.php
@@ -3,11 +3,57 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Brew Society</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Rouge+Script&family=Tangerine:wght@400;700&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
   </head>
   <body>
-    <h1>Welcome in Brew Society</h1>
+     <nav class="navbar navbar-expand-lg fixed-top bg-body-tertiary bg-dark px-5" data-bs-theme="dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Brew Society</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav mb-2 mb-lg-0 ms-auto">
+                <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#about">About Us</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="login.php">Login</a>
+                </li>
+            </ul>
+            </div>
+        </div>
+    </nav>
+
+    <section class="hero">
+        <div class="hero-content">
+            <h1>Welcome Coffeeholic</h1>
+            <p>Satu tempat, banyak rasa. Pilih kopi yang paling cocok buat mood kamu hari ini.</p>
+            <a class="btn btn-primary" href="login.php" role="button">Lihat Menu</a>
+          
+        </div>
+    </section>
+
+    <section id="about" class="about">
+      <section class="about-container">
+        <div class="about-deskripsi">
+          <h1>About Us</h1>
+          <p>Kami bekerja sama langsung dengan petani kopi lokal untuk memastikan kualitas terbaik</p>
+        </div>
+        <div class="about-img">
+          <img src="https://imgcdn.espos.id/@espos/images/2024/09/ilustrasi-kopi.jpg?quality=60" alt="kopi kita">
+        </div>
+      </section>
+
+    </section>
 
 
 
